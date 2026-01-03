b6d5d76bc2c1ef63d968bb1e2db348c7de51bd7e
jonadika <jona.dika@student.uni-pr.edu>
2026-01-03 17:56:48 +0100

    Create todoApp.cpp file and add functionality to show todo list

---
+++ todoApp.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    string todos [100];
+    string pergjigjja;
+    int numeruesi;
+    perseritja:
+    cout << "Cfare veprimi deshironi te kryeni?- Per te shiquar listen e todos, shtyp 'show'; Per te shtuar todos, shtyp 'Add';"
+    << "  Per te fshire/perfunduar todos shtyp 'Delete' " << endl;
+    cin >> pergjigjja;
+    while (pergjigjja != "exit"){
+        if (pergjigjja == "show") {
+            cout << "lista e todos eshte: "<< endl;
+            for(int i=0; i<100; i++){
+                cout << todos[i] << endl;
+            }
+        }
+        
+
+        goto perseritja;
+    }
+   return 0;
+}
